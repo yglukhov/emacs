f7e92511b6cb80dff8c6661680dd6b39cb81d79f
Yuriy Glukhov <yuriy.glukhov@gmail.com>
2020-04-18 23:25:41 +0300

    evalWithArgs

--- emacs/utils.nim
+++ emacs/utils.nim
@@ -2,17 +2,29 @@ import ../emacs
 import sugar
 
 proc eval*(s: string): Value =
-  ~eval(~car(~`read-from-string`(~format("(progn %s)", s))), 1)
+  let s = "(progn " & s & " )"
+  ~eval(~car(~`read-from-string`(s)), 1)
+
+proc evalWithArgs*(s: string, args: varargs[Value, toEmArg]): Value =
+  # Evals lisp code in `s`, which can refer to `args` as `$0`, `$1`, etc.
+  var e = "(lambda ("
+  for i in 0 ..< args.len:
+    e &= " $"
+    e &= $i
+  e &= ") "
+  e &= s
+  e &= " )"
+  let f = eval(e)
+  funcall(f, args)
 
 proc makeInteractiveFunction*(f: proc() {.nimcall.}): Value =
   # Converts `f` into an interactive function which can
   # be set as keybinding handler in emacs
-  let makeInteractiveFunction = eval("""
-  (lambda (f)
-    (let ((b f))
-      (lambda ()
-        "Some comment"
-        (interactive)
-        (funcall b)
-        )))""")
-  funcall(makeInteractiveFunction, f)
+  evalWithArgs("""
+  (let ((b $0))
+    (lambda ()
+      "Some comment"
+      (interactive)
+      (funcall b)
+      ))
+  """, f)
